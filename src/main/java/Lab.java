aefd78c95e9a78821e4a466320c21f7955d97162
Evan Benjamin <evanpbenjamin@cox.net>
2023-03-07 01:55:07 +0000

    Completed Lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -35,7 +35,10 @@ public class Lab {
     public int whileExample(int end){
         int sum = 0;
         int i = 0;
-
+        while(i<end){
+            sum+=i;
+            i++;
+        }
 //        code here
 
         return sum;
